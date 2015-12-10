326526948e8f897f06124f8f9b99c475cb759ab8
andrewchng <andrewchng92@gmail.com>
2015-12-10 13:45:14 +0800

    first commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var currentVal = 0
+    var maxVal = 0
+    var valueBefore = 0
+    
+    @IBOutlet weak var playBtn : UIButton!
+    @IBOutlet weak var logo : UIImageView!
+    @IBOutlet weak var multiplesTxt : UITextField!
+    @IBOutlet weak var addBtn : UIButton!
+    @IBOutlet weak var multiplesLbl : UILabel!
+    
+    @IBAction func addBtnPressed(sender: UIButton){
+        maxVal = 12 * Int(multiplesTxt.text!)!
+        currentVal += Int(multiplesTxt.text!)!
+        valueBefore  = currentVal - Int(multiplesTxt.text!)!
+        
+        multiplesLbl.text = "\(valueBefore) + \(Int(multiplesTxt.text!)!) = \(currentVal)"
+        
+        if gameOver() {
+            restartGame()
+        }
+        
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playBtnPressed(sender: UIButton){
+        if multiplesTxt.text != nil && multiplesTxt.text != "" {
+            logo.hidden = true
+            playBtn.hidden = true
+            multiplesTxt.hidden = true
+            addBtn.hidden = false
+            multiplesLbl.hidden = false
+            
+            multiplesLbl.text = "0"
+        }
     }
-
+    
+    func restartGame(){
+        logo.hidden = false
+        playBtn.hidden = false
+        multiplesTxt.hidden = false
+        addBtn.hidden = true
+        multiplesLbl.hidden = true
+        
+        valueBefore = 0
+        currentVal = 0
+        maxVal = 0
+    }
+    
+    func gameOver() -> Bool {
+        if currentVal == maxVal {
+            return true
+        }
+        else{
+            return false
+    
+        }
+    }
 
 }
 
